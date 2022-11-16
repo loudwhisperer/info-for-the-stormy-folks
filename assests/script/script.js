0aa6ee28d711e05f1b8c98c18d06ca15db539444
loudwhisperer <driftaway44@gmail.com>
2022-11-15 18:40:27 -0700

    added function convertDaily to getForecast in order to create the current weather card at the top

--- assests/script/script.js
+++ assests/script/script.js
@@ -3,29 +3,35 @@ $(function () {
   const searchBtn = $("#search");
   const rootEl = $("#root");
   const appId = "f73bdbc432694a653b9d129f40b58f7d";
-
+  let currentLoc = [];
   function searchManager(event) {
     event.preventDefault();
     const searchBox = $("#searchvalue").val();
-    let locationUrl =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      searchBox +
-      "&limit=5&appid=" +
-      appId +
-      "";
-    console.log(searchBox);
-      function getForecast(request) {
-        fetch(locationUrl).then(function(response) {
-          console.log(response);
-          if (response.status === 200){
-           const card = rootEl.append("div");
-           card.addClass("card text-center bg-info p-4 mx-3 my-1");
-           const cardHeader = card.append("h3")
-           cardHeader.text("Monday")
-          }
-        });
+    let locationUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchBox}&units=metric&appid=${appId}`;
+
+    async function getForecast(coordinates) {
+      const response = await fetch(locationUrl);
+      const data = await response.json();
+      console.log(data);
+      if (response.status === 200) {
+        currentLoc = data.city.coord;
+        let currentUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLoc.lat}&lon=${currentLoc.lon}&appid=${appId}`;
+        async function convertDaily() {
+          const response = await fetch(currentUrl);
+          const data = await response.json();
+          console.log(data);
+          const card = $("<div></div>");
+					$("<h2></h2>").text(data.name).appendTo(card);
+          $("<h4></h4>").text(dayjs(data.dt).format("MMMM D")).appendTo(card);
+          $("<p></p>").text(`${data.main.temp} °F`).appendTo(card);
+          $("<p></p>").text(`${data.main.humidity}%`).appendTo(card);
+          $("<p></p>").text(`${data.wind.speed} MPH`).appendTo(card);
+          rootEl.append(card);
+        }
+        convertDaily();
       }
-      getForecast(locationUrl)
+    }
+    getForecast(currentLoc);
   }
 
   searchBtn.on("click", searchManager);
